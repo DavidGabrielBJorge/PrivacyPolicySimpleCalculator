c16587a553b9f5c2835f3eef57f8a9d81596ec77
DavidGabrielBJorge <davidgbjorge@gmail.com>
2023-11-28 11:11:33 -0300

    add: adicionando carousel

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//utilizar o super slide 
+$(document).ready(function(){
+    //customizando o slide
+    $('#slides').superslides({
+        animation: 'slide',
+        play: 3000,
+        pagination: false
+    });
+});
