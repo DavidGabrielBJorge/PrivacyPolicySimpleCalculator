9968a2b3059e175d31a2f18bdfa2352370931f8c
DavidGabrielBJorge <davidgbjorge@gmail.com>
2023-11-29 09:12:43 -0300

    add: adicionando o texto sobre mim e a bilbioteca typed js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-//utilizar o super slide 
+//Ao inicializar a página os seguintes itens devem ser carregados
 $(document).ready(function(){
-    //customizando o slide
+    //Carregar o super silde
     $('#slides').superslides({
         animation: 'slide',
         play: 3000,
         pagination: false
     });
+
+    //Carrega o texto
+    var typed = new Typed('#element', {
+        strings: ['Analista de desenvolvimento', 'Programador Full Stack Júnior'],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1200
+      });
+
 });
+
